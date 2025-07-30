df74626ca9a24773b2cee1bf413d36c63f52bb4a
Huy Tuấn <163986199+hahuytuan033@users.noreply.github.com>
2025-07-30 17:06:49 +0700

    controller with animation

---
+++ Assets/Scripts/Controllers/CameraHandler.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Tundayne
+{
+    public class CameraHandler : MonoBehaviour
+    {
+        public TrackedReference camTrans;
+    }
+}
+
