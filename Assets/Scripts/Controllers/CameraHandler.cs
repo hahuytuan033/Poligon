eac8d7e0e694bc1178685d09b8d1f25c65f1ac74
Huy Tuấn <163986199+hahuytuan033@users.noreply.github.com>
2025-07-30 18:13:53 +0700

    CameraHandler and CameraaValue

--- Assets/Scripts/Controllers/CameraHandler.cs
+++ Assets/Scripts/Controllers/CameraHandler.cs
@@ -6,7 +6,75 @@ namespace Tundayne
 {
     public class CameraHandler : MonoBehaviour
     {
-        public TrackedReference camTrans;
+        public Transform camTrans;
+        public Transform target;
+        public Transform pivot;
+        public bool leftPivot;
+        float delta;
+
+        float mouseX;
+        float mouseY;
+        float smoothX;
+        float smoothY;
+        float smoothXVelocity;
+        float smoothYVelocity;
+        float lookAngle;
+        float tiltAngle;
+
+        public CameraValues cameraValues;
+
+        StatesManager statesManager;
+
+        public void Init(InputHandler inputHandler)
+        {
+            statesManager = inputHandler.statesManager;
+            target =  statesManager.transform;
+        }
+
+        public void FixedTick(float d)
+        {
+            delta = d;
+            if (target == null)
+            {
+                return;
+            }
+
+            HandlePosition();
+        }
+
+        void HandlePosition()
+        {
+            float targetX = cameraValues.normalX;
+            float targetZ = cameraValues.normalZ;
+            float targetY = cameraValues.normalY;
+
+            if (statesManager.controllerStates.isCrouching)
+            {
+                targetY = cameraValues.crouchY;
+            }
+
+            if (statesManager.controllerStates.isAiming)
+            {
+                targetX = cameraValues.aimX;
+                targetZ = cameraValues.aimZ;
+            }
+
+            if (leftPivot)
+            {
+                targetX = -targetX;
+            }
+
+            Vector3 newPivpotPos = pivot.localPosition;
+            newPivpotPos.x = targetX;
+            newPivpotPos.z = targetY;
+
+            Vector3 newCampos = camTrans.localPosition;
+            newCampos.z = targetZ;
+
+            float t = delta * cameraValues.adaptSpeed;
+            pivot.localPosition = Vector3.Lerp(pivot.localPosition, newPivpotPos, t);
+            camTrans.localPosition = Vector3.Lerp(camTrans.localPosition, newCampos, t);
+        }
     }
 }
 
---
+++ Assets/Scripts/Controllers/CameraValues.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+
+namespace Tundayne
+{
+    public class CameraValues : ScriptableObject
+    {
+        public float turnSmooth = 0.1f;
+        public float moveSpeed = 9f;
+        public float y_rotation_speed = 8f;
+        public float x_rotation_speed = 8f;
+        public float minAngle = -35f;
+        public float maxAngle = 35f;
+        public float normalZ = -3f;
+        public float normalX;
+        public float aimZ = -0.5f;
+        public float aimX = 0;
+        public float normalY;
+        public float crouchY;
+        public float adaptSpeed = 9f;
+    }
+}
+
